91b17e066a25619d3526b08c8dd85a4e59ab455a
davitmartirosyan <phpencoding@gmail.com>
2024-08-16 10:49:53 +0400

    rust00

--- rust00/main.rs
+++ rust00/main.rs
@@ -1,12 +1,8 @@
-
 mod ex00;
 
 fn main()
 {
-	ex00::r_putchar::r_putchar('D');
-	ex00::r_putchar::r_putchar('a');
-	ex00::r_putchar::r_putchar('v');
-	ex00::r_putchar::r_putchar('i');
-	ex00::r_putchar::r_putchar('t');
+	//testing environment
+	ex00::r_putchar::r_putchar('A');
 	ex00::r_putchar::r_putchar('\n');
 }
